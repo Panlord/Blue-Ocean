fd3f4df84416316b40921a60aa290bd384ea0450
Camden Smith <camdensmithh@gmail.com>
2022-08-12 10:39:26 -0700

    Initial build

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.json());
+
+app.use('/', routes);
+
+app.listen(3001, () => {
+  console.log('Server is running at port 3001');
+});
