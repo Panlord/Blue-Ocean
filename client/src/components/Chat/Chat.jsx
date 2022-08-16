5fc58d15fba102fe3e223650ae3d36aa72614369
Panlord <adpan.dev@gmail.com>
2022-08-15 18:28:40 -0700

    initialize chat component of the client

---
+++ client/src/components/Chat/Chat.jsx
@@ -0,0 +1,14 @@
+// Import stuff
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+export default function Chat (props) => {
+  const [listOfMessages, setListOfMessages] = useState([]);
+  const [message, setMessage] = useState('');
+
+  return (
+    <div>
+
+    </div>
+  );
+};
