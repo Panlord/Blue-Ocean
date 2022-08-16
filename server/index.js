2bf35b0339aa6693f5c9d235f3c93df028b1706c
Panlord <adpan.dev@gmail.com>
2022-08-15 21:33:47 -0700

    initialize socket server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');
-
-
 const app = express();
+
+// socket.io server stuff
+const http = require('http');
+const { Server } = require('socket.io');
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+const routes = require('./routes');
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
