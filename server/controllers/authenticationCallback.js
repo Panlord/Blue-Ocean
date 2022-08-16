569da19ec9f2ec3aa77aadc4ff356d54bae8ae31
Nick Johnson <ntjohns10@gmail.com>
2022-08-16 13:56:34 -0700

    added username to app state

--- server/controllers/authenticationCallback.js
+++ server/controllers/authenticationCallback.js
@@ -2,12 +2,12 @@ var models = require ('./../models')
 
 module.exports = {
   get : (req, res ) => {
-    console.log(req.query.state, 'state item');
+    console.log(req.query.state, 'state item'); // user has state
     models.authenticationCallback(req.query.code, (err, results) => {
       if (err) {
         console.log(err, 'auth error');
       } else {
-        res.redirect('/');
+        res.redirect(`/`);
       }
     })
   }
